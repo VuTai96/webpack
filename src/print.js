f45c0924debbbf503737f48b58240582dfda190a
VuTai96 <vutai13196@gmail.com>
2025-04-24 17:47:32 +0700

    development: 1

--- src/print.js
+++ src/print.js
@@ -1,7 +1,3 @@
 export default function printMe() {
-    console.log('I get called from print.js!');
-}
-
-export function sum(a, b) {
-    return a + b;
+    console.log('I get called  ssdfafrom print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watch } = require('fs');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     entry: {
         index: './src/index.js',
         print: './src/print.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Development',
         }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };

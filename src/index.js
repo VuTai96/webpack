bf57da5119630212727fc46d9d7470e8c41a15fe
VuTai96 <vutai13196@gmail.com>
2025-04-26 15:31:20 +0700

    codeSplitting: Dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const _ = require('lodash');
-
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+function getComponent() {
+    return import('lodash')
+        .then(({ default: _ }) => {
+            const element = document.createElement('div');
+            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+            return element;
+        })
+        .catch((error) => 'An error occurred while loading the component: ' + error.message);
 }
-
-document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});

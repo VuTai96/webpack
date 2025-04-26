68e3b966a84004db6fe46569ebfd814c7f6d5fd5
VuTai96 <vutai13196@gmail.com>
2025-04-26 11:51:39 +0700

    codeSplitting - Prevent duplication using entry dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,22 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };

a3a33aa43aae5a8e81d1bea079e2147ab8eb8d33
VuTai96 <vutai13196@gmail.com>
2025-04-28 10:52:58 +0700

    caching: module identifiers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const _ = require('lodash');
+import _ from 'lodash';
+// import print from './print';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.onclick = print.bind(null, 'Hello webpack!');
 
     return element;
 }

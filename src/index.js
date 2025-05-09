ca22f386f9af91cfe9a41b2c6d184b9f6982175f
VuTai96 <vutai13196@gmail.com>
2025-05-09 11:46:00 +0700

    treeShaking - apply for de

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const _ = require('lodash');
+import { cube } from './math.js';
 
 function component() {
-    const element = document.createElement('div');
+    const element = document.createElement('pre');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n');
 
     return element;
 }

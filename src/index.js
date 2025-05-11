30ef703384568614124c5887cb42bc4edcdd6ac4
VuTai96 <vutai13196@gmail.com>
2025-05-11 16:43:49 +0700

    production: config to dev, prod

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const _ = require('lodash');
+import _ from 'lodash';
+    
+if (process.env.NODE_ENV == 'development') {
+    console.log('Looks like we are in development mode!');
+  }
+
+  if (process.env.NODE_ENV == 'production') {
+    console.log('Looks like we are in production mode!');
+  }
 
 function component() {
+    console.log(process.env.NODE_ENV )
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'Taif vux'], ' ');
 
     return element;
 }
